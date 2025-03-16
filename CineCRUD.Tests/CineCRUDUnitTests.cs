3fc4f3d3eba095d0238b5fa4fa581abce0987b89
Gabriel <gabrielgmarcolin12@gmail.com>
2025-03-16 15:21:31 -0300

    Alterado nome do arquivo de testes de unidade

--- CineCRUD.Tests/CineCRUDUnitTests.cs
+++ CineCRUD.Tests/CineCRUDUnitTests.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 namespace CineCRUD.Tests
 {
     [TestClass]
-    public class CineCRUDTests
+    public class CineCRUDUnitTests
     {
         [TestMethod]
         public void inicializarDataTableDeveCriarColunasCorretamente()
